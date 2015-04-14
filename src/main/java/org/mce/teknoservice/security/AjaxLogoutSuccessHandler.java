e1e719bde083995e51a641ab057cdcc36864cb06
MarcoCeccarini <ceccarinimarco@gmail.com>
2015-04-14 11:08:45 +0200

    Signed-off-by: MarcoCeccarini <ceccarinimarco@gmail.com>

--- src/main/java/org/mce/teknoservice/security/AjaxLogoutSuccessHandler.java
+++ src/main/java/org/mce/teknoservice/security/AjaxLogoutSuccessHandler.java
@@ -1,14 +1,19 @@
 package org.mce.teknoservice.security;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mce.teknoservice.config.AuditEventPublisher;
+import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AbstractAuthenticationTargetUrlRequestHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Spring Security logout handler, specialized for Ajax requests.
@@ -16,11 +21,19 @@ import java.io.IOException;
 @Component
 public class AjaxLogoutSuccessHandler extends AbstractAuthenticationTargetUrlRequestHandler
         implements LogoutSuccessHandler {
+	
+	@Inject
+    private AuditEventPublisher auditPublisher;
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
                                 Authentication authentication)
             throws IOException, ServletException {
+    	
+    	String principal = authentication.getName();
+    	AuditEvent event = new AuditEvent(principal, "LOGOUT_END", new HashMap<String, Object>());
+    	auditPublisher.publish(event);
+    	
         response.setStatus(HttpServletResponse.SC_OK);
     }
 }
